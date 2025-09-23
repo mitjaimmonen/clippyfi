a33651108290976bb5e1f6f2fac634495d3829fc
mitja <mitja.immonen@protonmail.com>
2025-09-23 19:30:42 +0300

    feat: link to clippy video

--- website/src/components/HelloWorld.vue
+++ website/src/components/HelloWorld.vue
@@ -9,28 +9,30 @@
 </script>
 
 <template>
-  <v-container class="fill-height fluid">
-    <v-row align="center" justify="center">
-      <v-col align="center" justify="center">
-        <img alt="logo" class="py-4" src="@/assets/clippy.png" width="80">
-        <div>
-          <v-card
-            class="py-4 my-4 shrink-card"
-            color="surface-variant"
-            rounded="lg"
-            variant="tonal"
-          >
-            <v-card-title>
-              <h2 class="text-h5 font-weight-bold">
-                <span v-if="prompt != null">{{ prompt }}</span>
-                <span v-else> Clippy is not happy about the state of the world.</span>
-              </h2>
-            </v-card-title>
-          </v-card>
-        </div>
-      </v-col>
-    </v-row>
-  </v-container>
+  <a href="https://youtu.be/2_Dtmpe9qaQ?feature=shared" style="text-decoration: none;" target="_blank">
+    <v-container class="fill-height fluid">
+      <v-row align="center" justify="center">
+        <v-col align="center" justify="center">
+          <img alt="logo" class="py-4" src="@/assets/clippy.png" width="80">
+          <div>
+            <v-card
+              class="py-4 my-4 shrink-card"
+              color="surface-variant"
+              rounded="lg"
+              variant="tonal"
+            >
+              <v-card-title>
+                <h2 class="text-h5 font-weight-bold">
+                  <span v-if="prompt != null">{{ prompt }}</span>
+                  <span v-else> Clippy is not happy about the state of the world.</span>
+                </h2>
+              </v-card-title>
+            </v-card>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </a>
 </template>
 
 <style scoped>
