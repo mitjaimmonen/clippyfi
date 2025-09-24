35e65fecd0e3573d865ebdf73c4747996efae88d
mitja <mitja.immonen@protonmail.com>
2025-09-24 11:33:26 +0300

    feat: serve website through server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,26 @@
 import express, { Request, Response } from 'express';
+import path from 'path';
 
 const app = express();
 const port = 3000;
 
+// ------- Serve Vue frontend -------- //
+
+// public = Vue dist
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+// Fallback route for SPA
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// ------- Serve API -------- //
+
 app.get('/api/prompt', (req: Request, res: Response) => {
   res.json({ message: 'Clippy would never show advertisements on your fridge.' });
 });
+
+// ------- Listen -------- //
 
 app.listen(port, () => {
   const env = process.env.NODE_ENV as 'production' | 'development' | 'test' | undefined;
